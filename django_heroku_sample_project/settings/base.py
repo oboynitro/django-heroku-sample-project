0a5403a3c01bf3ead2c747059d27042313c0dea8
Frank Owusu Boakye <oboy.nitro1@gmail.com>
2021-12-31 10:06:23 +0000

    update settings

--- django_heroku_sample_project/settings/base.py
+++ django_heroku_sample_project/settings/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -57,7 +58,7 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR / "media"
 
