735f514151aed3d759438279a62dfda4df8ff376
Frank Owusu Boakye <oboy.nitro1@gmail.com>
2021-12-31 09:42:03 +0000

    collected static files

--- django_heroku_sample_project/settings/local.py
+++ django_heroku_sample_project/settings/local.py
@@ -6,6 +6,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 WSGI_APPLICATION = 'django_heroku_sample_project.wsgi.local.application'
 
 DATABASES = {
--- django_heroku_sample_project/settings/production.py
+++ django_heroku_sample_project/settings/production.py
@@ -8,6 +8,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["example-app.herokuapp.com"]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 WSGI_APPLICATION = 'django_heroku_sample_project.wsgi.production.application'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
